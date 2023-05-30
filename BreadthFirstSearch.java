b3b9e41b37f3deb22f2900901fd0cf55fbfccb94
Nishovec <shaymardanov_E@inbox.ru>
2023-05-30 21:58:11 +0600

    a

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -3,4 +3,27 @@ class BreadthFirstSearch<V> implements Search<V> {
 
     public BreadthFirstSearch(WeightedGraph<V> graph) {
         this.graph = graph;
-    }
+
+    }@Override
+    public List<Vertex<V>> traverse(Vertex<V> source) {
+        List<Vertex<V>> result = new ArrayList<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        queue.add(source);
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> current = queue.poll();
+            result.add(current);
+
+            List<Vertex<V>> neighbors = graph.getAdjacentVertices(current);
+            for (Vertex<V> neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+
+        return result;
