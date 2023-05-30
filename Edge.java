f324582c4ade1169e25b620e3feba8dacd1a1f28
Nishovec <shaymardanov_E@inbox.ru>
2023-05-30 21:41:04 +0600

    a

--- Edge.java
+++ Edge.java
@@ -2,4 +2,12 @@ public class Edge<Vertex> {
     private Vertex source;
     private Vertex dest;
     private Double weight;
+
+    public Edge(Vertex source,
+                Vertex dest,
+                Double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
 }
